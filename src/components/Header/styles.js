d9762c6b907198837d32c20a9d8ed1d879bd015a
Everson Silva <everson_gsilva@yahoo.com.br>
2021-09-10 14:08:45 -0300

    style: create styles component Header

---
+++ src/components/Header/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  padding: 20px;
+  margin-top: 20px;
+  background-color: #008956;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderTitle = styled.Text`
+  font-family: 'Roboto_700Bold';
+  color: #fff;
+  font-size: 18px;
+`;
