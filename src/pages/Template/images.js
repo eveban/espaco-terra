9e0e4b25310e2bc99e98fda8d255bd0fa51cac85
Everson Silva <everson_gsilva@yahoo.com.br>
2021-09-10 14:07:54 -0300

    feat: import images in module templates

---
+++ src/pages/Template/images.js
@@ -0,0 +1,36 @@
+/* eslint-disable global-require */
+
+const images = [
+  {
+    source: require('../../assets/templates/1.png'),
+    width: 1080,
+    height: 1920,
+  },
+  {
+    source: require('../../assets/templates/2.png'),
+    width: 1080,
+    height: 1920,
+  },
+  {
+    source: require('../../assets/templates/3.png'),
+    width: 1080,
+    height: 1920,
+  },
+  {
+    source: require('../../assets/templates/4.png'),
+    width: 1080,
+    height: 1920,
+  },
+  {
+    source: require('../../assets/templates/5.png'),
+    width: 1080,
+    height: 1920,
+  },
+  {
+    source: require('../../assets/templates/6.png'),
+    width: 1080,
+    height: 1920,
+  },
+];
+
+export default images;
