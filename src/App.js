2d340ee37e65eb0acc90fc9b77668a6c05d6638c
Everson Silva <everson_gsilva@yahoo.com.br>
2021-09-10 13:58:04 -0300

    chore: add fonts expo, StatusBar color and routes in the project

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'react-native';
+import {
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
+
+import Routes from './routes';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+    Ubuntu_700Bold,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  return (
+    <>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="#008956"
+        networkActivityIndicatorVisible
+        animated
+      />
+      <Routes />
+    </>
+  );
+}
